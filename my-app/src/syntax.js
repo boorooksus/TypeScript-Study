4db9a6b14ea0dfba8358507515a5609f24abd525
boorooksus <boorooksus@gmail.com>
2022-03-16 13:37:23 +0900

    리액트 추가

---
+++ my-app/src/syntax.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+function syntax() {
+    var 이름 = "abc";
+    var 타입불확실 = { name: "kim" };
+    var 어떤거 = 123;
+    function 함수(x) {
+        return x * 2;
+    }
+    var john = [123, true];
+    var ann = { name: "kim", age: "123" };
+    return <div>{이름}</div>;
+}
+exports.default = syntax;
---
+++ my-app/src/syntax.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function syntax() {
+  let 이름: string = "abc";
+
+  return <div>{이름}</div>;
+}
